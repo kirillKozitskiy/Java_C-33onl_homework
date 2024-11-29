2617f35b49b290e28ddf85c79184a42ebbc61347
kirillKozitskiy <kkozickij35@gmail.com>
2024-11-29 22:09:19 +0300

    lisson2

---
+++ src/lesson2/homework/TheSecondBlockPartOne.java
@@ -0,0 +1,171 @@
+package lesson2.homework;
+
+import java.util.Scanner;
+
+public class TheSecondBlockPartOne {
+    public static void main(String[] args) throws InterruptedException {
+
+        /*Information for Archil: the main method throws exceptions because
+        I use the sleep method in the secondTask for fun.
+        */
+
+
+        //firstTask();
+
+        //secondTask();
+
+        //thirdTask();
+
+        //fourthTask();
+
+        //fifthTask();
+
+        //taskWithStarSymbol();
+
+
+    }
+
+
+    public static void firstTask(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input first value:\t");
+        int firstValue = sc.nextInt();
+        System.out.print("Input the second value:\t");
+        int secondValue = sc.nextInt();
+
+        int result = 4*(firstValue+secondValue-1)/2;
+
+        System.out.println("Your result of the equation is "+result);
+
+
+
+    }
+
+    public static void secondTask() throws InterruptedException {
+        System.out.println("THE SECOND TASK");
+
+        Scanner sc = new Scanner(System.in);
+        boolean validInput = false;
+
+        System.out.println("Enter a two-digit number:\t");
+
+        while(!validInput) {
+            int number = sc.nextInt();
+
+            if (9 < number && number < 99) {
+                System.out.println("Calculating...\nPlease wait a bit");
+                Thread.sleep(4000);
+
+                int result = (number / 10 + number % 10);
+
+                System.out.println("The sum of digits in your number is " + result);
+                validInput = true;
+            } else {
+                System.out.println("You've entered an invalid number\nTry again");
+
+            }
+        }
+        sc.close();
+
+
+
+
+    }
+
+    public static void thirdTask(){
+        System.out.println("THE THIRD TASK");
+
+        Scanner sc = new Scanner(System.in);
+        boolean validInput = false;
+
+        System.out.println("Enter a three digit number:\t");
+
+        while (!validInput) {
+
+
+            int number = sc.nextInt();
+
+            if (99 < number && number < 999) {
+                int result = 0;
+
+                while (number > 0) {
+                    result += number % 10;
+                    number /= 10;
+                }
+
+                System.out.println("The sum of digits in your number is " + result);
+                validInput = true;
+            } else {
+                System.out.println("You've entered an invalid number\nTry again");
+
+            }
+        }
+
+
+
+
+
+
+    }
+
+    public static void fourthTask(){
+        System.out.println("THE FOURTH TASK");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a fractional number to round:\t ");
+
+        double fractionalNumber = sc.nextDouble();
+
+        int roundValue = (int) Math.round(fractionalNumber);
+
+        System.out.println("Your round value is "+roundValue);
+    }
+
+    public static void fifthTask(){
+        System.out.println("THE FIFTH TASK");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first value:\t");
+        int firstValue = sc.nextInt();
+
+        System.out.println("Enter the second value:\t");
+        int secondValue = sc.nextInt();
+
+        int remainder = firstValue%secondValue;
+        int result = firstValue/secondValue;
+
+        System.out.println(firstValue+" / "+secondValue+" = "+result+" and "+remainder+" left");
+
+    }
+
+    public static void taskWithStarSymbol(){
+        System.out.println("THE STAR SYMBOL TASK");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two values and I'll switch them around:\t");
+
+        int firstValue = sc.nextInt();
+        int secondValue = sc.nextInt();
+
+        System.out.println("first value is "+firstValue);
+        System.out.println("second value is "+secondValue+"\n");
+        //solution without third value
+        firstValue = firstValue + secondValue;
+        secondValue = firstValue - secondValue;
+        firstValue = firstValue - secondValue;
+
+        System.out.println("first value is "+firstValue);
+        System.out.println("second value is "+secondValue);
+
+        /* solution with third value
+          int b;
+          b = one;
+          one = two;
+          two = b;
+         */
+
+    }
+
+}
---
+++ src/lesson2/homework/TheSecondBlockPartTwo.java
@@ -0,0 +1,109 @@
+package lesson2.homework;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TheSecondBlockPartTwo {
+    public static void main(String[] args) {
+
+        //firstTask();
+
+        //secondTask();
+
+        //thirdTask();
+
+        //fourthTask();
+
+        fifthTaskWithStarSymbol();
+
+
+
+
+    }
+
+
+    public static void firstTask(){
+
+        System.out.println("Enter the number: ");
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num % 2 == 0)
+        {
+            System.out.println("The number is even");
+        }
+        else
+            System.out.println("The number is odd");
+    }
+
+    public static void secondTask(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter street temperature:\t");
+
+        int temperature = sc.nextInt();
+
+        if(temperature > -5)
+            System.out.println("Warm");
+        else if (temperature <= -5 && temperature > -20)
+            System.out.println("Normal");
+        else
+            System.out.println("Cold");
+
+        sc.close();
+    }
+
+    public static void thirdTask(){
+
+        for(int i = 10;  i<=20; i++){
+            System.out.println(i*i+"\n");
+        }
+    }
+
+    public static void fourthTask(){
+
+        int value = 7;
+        int helpValue = 0;
+        while (helpValue<98){
+            System.out.print((helpValue+=value)+" ");
+        }
+    }
+
+
+    /*
+    I use try catch to prevent an exception.
+    We haven't had this lesson yet, but I am familiar with this design and in assignment says that we need
+    to do a check to make sure that the user has not entered incorrect data(we need to enter Integer)
+     */
+
+    public static void fifthTaskWithStarSymbol(){
+        Scanner sc = new Scanner(System.in);
+        boolean flag = false;
+        int sum = 0;
+
+        try {
+            while(!flag){
+                System.out.println("Enter the number: ");
+                int value = sc.nextInt();
+                if(value>0){
+                    for(int i = 0; i<=value; i++){
+                        sum+=i;
+                    }
+                    System.out.println("The sum is: "+sum);
+                    flag = true;
+                }
+                else {
+                    System.out.println("You entered negative number. Try again.");
+                }
+            }
+
+        }
+        catch (InputMismatchException e){
+            System.out.println("You entered a non integer number. Try again.");
+        }
+
+
+
+    }
+
+}
