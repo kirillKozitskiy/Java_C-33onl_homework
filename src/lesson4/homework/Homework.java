a733d5d083484ad51a2e9cd751d47d573d864e15
kirillKozitskiy <kkozickij35@gmail.com>
2024-12-02 20:41:52 +0300

    update lesson4

--- src/lesson4/homework/Homework.java
+++ src/lesson4/homework/Homework.java
@@ -1,5 +1,6 @@
 package lesson4.homework;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Homework {
@@ -12,13 +13,9 @@ public class Homework {
         int[] array = createArray(5);
 
         fillArrayWithRandom(array);
+        //printArray(array);
 
-       printArray(array);
-       printArrayInReverse(array);
-
-       sortArray(array);
-        System.out.println("------------\n");
-       printArray(array);
+        printArray2(array);
 
 
     }
@@ -57,9 +54,72 @@ public class Homework {
 
     }
 
+    public static void searchForMinElement(int[] array){
+        int min = array[0];
+        int index = 0;
+        for(int i = 0; i<array.length; i++){
+            if(array[i] < min){
+                min = array[i];
+                index = i;
+            }
+        }
+        System.out.println("Min element is "+min+"\nIndex of min is "+index);
+    }
+
     public static void searchForMaxElement(int[] array){
+        int max = array[0];
+        int index = 0;
+        for(int i = 0; i <array.length; i++){
+            if(array[i]>max){
+                max = array[i];
+                index = i;
+            }
 
+        }
+        System.out.println("Max element is "+max+"\nIndex of max is "+index);
+    }
 
+    public static void searchForZeroElement(int[] array){
+        int counter = 0;
+        for(int current: array){
+            if(current==0){
+                counter++;
+            }
+        }
+        if(counter==0){
+            System.out.println("We have no zero elements in the array");
+        }
+        else
+            System.out.println("We have "+counter+" zero elements in the array");
+
+    }
+
+    public static void swapPlaces(int[] array){
+        
+        int size = array.length;
+        
+        for(int i = 0; i < size/2; i++){
+            int temp = array[i];
+            array[i] = array[size-1-i];
+            array[size-1-i] = temp;
+        }
+    }
+
+    public static boolean isIncrementalArray(int[] array){
+        for(int i = 1; i<array.length; i++){
+            if(array[i]<=array[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void incrementCheck(int[] array){
+        boolean isIncrement = isIncrementalArray(array);
+        if(isIncrement)
+            System.out.println("Array is incremental");
+        else
+            System.out.println("Array is not incremental");
 
 
     }
@@ -82,6 +142,25 @@ public class Homework {
         }
 
     }
+
+    public static void printArray2(int[] array){
+
+        int n = array.length;
+
+        for (int i = 0; i < n; i++) {
+            if(array[n-i]<9){
+                array[n-i]++;
+                break;
+            }
+
+        }
+
+
+    }
+
+
+
+
 
 
 
