f75a343d260c0e2a601b7b4c9c780658888de345
kirillKozitskiy <kkozickij35@gmail.com>
2025-01-25 02:20:44 +0300

    First Task of lesson 12 has been improved, exactly has been added a List to save abbreviatures

--- src/lesson12/homework/MyMain.java
+++ src/lesson12/homework/MyMain.java
@@ -1,6 +1,8 @@
 package lesson12.homework;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,9 +10,20 @@ import java.util.regex.Pattern;
 public class MyMain {
     public static void main(String[] args) {
 
-        System.out.println("Please, enter the line to determine if it's an abbreviation.");
+        System.out.println("\u001B[34mPlease, enter the line to determine if it's an abbreviation.\u001B[0m");
         String line = inputLine().nextLine();
+
+        List<String> abbreviations = returnAbbreviationWordsFromTheLine(line);
+        System.out.println();
+        System.out.println("\u001B[34mList of abbreviations\u001B[0m");
+        abbreviations.forEach(System.out::println);
+
+        inputLine().close();
+    }
+
+    public static List<String> returnAbbreviationWordsFromTheLine(String line) {
         String[] words = line.split(" ");
+        List<String> abbreviations = new ArrayList<>();
 
         Pattern isAbbriviationPattern = Pattern.compile("^[A-Z]{2,6}");
 
@@ -18,18 +31,13 @@ public class MyMain {
             Matcher isAbbriviationMatcher = isAbbriviationPattern.matcher(word);
             if(isAbbriviationMatcher.matches()) {
                 System.out.println("It IS an abbreviation -> " + word);
+                abbreviations.add(word);
             }
             else {
                 System.out.println("It ISN'T an abbreviation -> " + word);
             }
         }
-        inputLine().close();
-
-
-
-
-
-
+        return abbreviations;
     }
 
     public static Scanner inputLine(){
